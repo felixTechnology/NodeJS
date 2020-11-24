744fd8de732162205bf8b79060a786be4d305db7
Felix Atsu Kofi Ackuayi <felixackuayi@aol.com>
2020-11-24 14:20:09 +0000

    routing request - Load a page where USER Enter a data which is stored in a file

--- serverNodeStart/app.js
+++ serverNodeStart/app.js
@@ -27,3 +27,35 @@ const server = http.createServer(function (req, res) {
 server.listen(3000);*/
 
 /************************************************************************************************************************/
+/*webserver that does different things depending on which route we enter*/
+const http = require('http');
+
+const server = http.createServer(function (req, res) {
+
+    //console.log(req)
+    //console.log(req.url, req.method, req.headers);
+
+    const url = req.url;
+    if(url === '/'){
+        res.setHeader('content-Type','text/html');
+        res.write(`<html>`)
+        res.write(`<head><title>Enter Message</title></head>`)
+        res.write(`<body><h1><form action="/message" method="POST"><input type="text" name="message"><button type="button">Send</button></form></h1></body>`)
+        res.write(`</html>`)
+
+         return res.end();
+    }
+    res.setHeader('content-Type','text/html');
+    res.write(`<html>`)
+    res.write(`<head><title>My FirstPage</title></head>`)
+    res.write(`<body><h1>Hello from my Node.js</h1></body>`)
+    res.write(`</html>`)
+
+    res.end();
+
+//process.exit();
+});
+
+server.listen(3000);
+
+
